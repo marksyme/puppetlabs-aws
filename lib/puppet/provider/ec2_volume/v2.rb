ca82fab1e5173f6d2948a717a8da902d8c2877df
marksyme <mark.syme@moneysupermarket.com>
2017-08-07 11:30:04 +0100

    testing

--- lib/puppet/provider/ec2_volume/v2.rb
+++ lib/puppet/provider/ec2_volume/v2.rb
@@ -30,15 +30,6 @@ Puppet::Type.type(:ec2_volume).provide(:v2, :parent => PuppetX::Puppetlabs::Aws)
     end
   end
 
-  def self.volume_to_hash(region, volume)
-    name = name_from_tag(volume)
-    attachments = volume.attachments.collect do |att|
-      {
-        instance_id: att.instance_id,
-        device: att.device,
-        delete_on_termination: att.delete_on_termination
-      }
-    end
     config = {
       name: name,
       volume_id: volume.volume_id,
@@ -57,12 +48,6 @@ Puppet::Type.type(:ec2_volume).provide(:v2, :parent => PuppetX::Puppetlabs::Aws)
   def exists?
     Puppet.info("Checking if EC2 volume #{name} exists in region #{target_region}")
     @property_hash[:ensure] == :present
-  end
-
-  def create_from_snapshot(config)
-    snapshot = resource[:snapshot_id] ? resource[:snapshot_id] : false
-    config['snapshot_id'] = snapshot if snapshot
-    config
   end
 
   def ec2
@@ -85,43 +70,5 @@ Puppet::Type.type(:ec2_volume).provide(:v2, :parent => PuppetX::Puppetlabs::Aws)
       config[:block_device_mappings] = [{ :device_name=> resource[:attach]["device"], :ebs=> { :delete_on_termination=> true } }]
       ec2.modify_instance_attribute(config)
     end
-  end
-
-  def create
-    Puppet.info("Creating Volume #{name} in region #{target_region}")
-    config = {
-      size: resource[:size],
-      availability_zone: resource[:availability_zone],
-      volume_type: resource[:volume_type],
-      iops: resource[:iops],
-      encrypted: resource[:encrypted],
-      kms_key_id: resource[:kms_key_id],
-    }
-
-    config = create_from_snapshot(config)
-    response = ec2.create_volume(config)
-
-    ec2.create_tags(
-      resources: [response.volume_id],
-      tags: tags_for_resource
-    ) if resource[:tags]
-
-    attach_instance(response.volume_id) if resource[:attach]
-
-    @property_hash[:id] = response.volume_id
-    @property_hash[:ensure] = :present
-  end
-
-  def destroy
-    Puppet.info("Deleting Volume #{name} in region #{target_region}")
-    # Detach if in use first
-    config = {
-      volume_id: volume_id,
-      force: true
-    }
-    ec2.detach_volume(config) unless @property_hash[:attach] == nil
-    ec2.wait_until(:volume_available, volume_ids: [volume_id])
-    ec2.delete_volume(volume_id: volume_id)
-    @property_hash[:ensure] = :absent
   end
 end
